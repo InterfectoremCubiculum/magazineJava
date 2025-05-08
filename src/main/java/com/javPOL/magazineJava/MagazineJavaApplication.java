8e92fd2a15fae741279a624f51920d02c2e7e6f2
InterfectoremCubiculum <eryksliwowskic@gmail.com>
2025-05-08 16:05:06 +0200

    Initial commit

---
+++ src/main/java/com/javPOL/magazineJava/MagazineJavaApplication.java
@@ -0,0 +1,13 @@
+package com.javPOL.magazineJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazineJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javPOL/magazineJava/MagazineJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javPOL.magazineJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazineJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
