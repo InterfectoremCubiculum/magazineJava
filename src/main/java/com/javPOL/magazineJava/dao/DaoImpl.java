aba53bbdce06c1a6c0485d87126eea34a254e0fe
Eryk Śliwowski <eryksliwowskic@gmail.com>
2025-05-15 13:07:43 +0200

    dao interface, abstract class

---
+++ src/main/java/com/javPOL/magazineJava/dao/DaoImpl.java
@@ -0,0 +1,51 @@
+package com.javPOL.magazineJava.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.util.List;
+
+public abstract class DaoImpl<T, ID> implements Dao<T,ID> {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private final Class<T> entityClass;
+
+    public DaoImpl(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+    @Override
+    public void save(Object entity) {
+        entityManager.persist(entity);
+    }
+
+    @Override
+    public void update(Object entity) {
+        entityManager.merge(entity);
+    }
+
+    @Override
+    public void delete(Object entity) {
+        entityManager.remove(entity);
+    }
+
+    @Override
+    public T findById(ID id) {
+        return entityManager.find(entityClass, id);
+    }
+
+    @Override
+    public List<T> findAll() {
+        return entityManager
+                .createQuery("SELECT e FROM" + entityClass.getName() + " e", entityClass)
+                .getResultList();
+    }
+
+    @Override
+    public long count() {
+        return entityManager
+                .createQuery("SELECT COUNT(*) FROM " + entityClass.getName(), Long.class)
+                .getSingleResult();
+    }
+}
