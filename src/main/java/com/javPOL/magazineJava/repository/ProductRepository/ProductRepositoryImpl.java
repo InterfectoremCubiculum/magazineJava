788b8fe1da78e3e149a57390c85b6cf18e4c0d8a
Eryk Śliwowski <eryksliwowskic@gmail.com>
2025-05-22 02:31:29 +0200

    - added more save sql query option

--- src/main/java/com/javPOL/magazineJava/repository/ProductRepository/ProductRepositoryImpl.java
+++ src/main/java/com/javPOL/magazineJava/repository/ProductRepository/ProductRepositoryImpl.java
@@ -5,15 +5,16 @@ import com.javPOL.magazineJava.model.Product;
 import com.javPOL.magazineJava.repository.RepositoryImpl;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
-import static com.javPOL.magazineJava.util.OrderByClauseBuilder.buildOrderByClause;
-
 @Repository
 public class ProductRepositoryImpl extends RepositoryImpl<Product, Long> implements ProductRepository {
     @PersistenceContext
@@ -25,16 +26,23 @@ public class ProductRepositoryImpl extends RepositoryImpl<Product, Long> impleme
 
     @Override
     public Page<Product> findAll(Pageable pageable, Long categoryId) {
-        String orderByClause = buildOrderByClause(pageable);
-        String baseQuery = "SELECT e FROM " + Product.class.getName() + " e" + " WHERE e.category.id = :categoryId";
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> cr = cb.createQuery(Product.class);
+        Root<Product> root = cr.from(Product.class);
+        Predicate categoryPredicate = cb.equal(root.get("category").get("id"), categoryId);
+        cr.select(root).where(categoryPredicate);
+
         List<Product> content = entityManager
-                .createQuery(baseQuery + orderByClause, Product.class)
-                .setParameter("categoryId", categoryId)
+                .createQuery(cr)
                 .setFirstResult((int) pageable.getOffset())
                 .setMaxResults(pageable.getPageSize())
                 .getResultList();
 
-        long count = count();
+        CriteriaQuery<Long> countQuery = cb.createQuery(Long.class);
+        Root<Product> countRoot = countQuery.from(Product.class);
+        countQuery.select(cb.count(countRoot)).where(cb.equal(countRoot.get("category").get("id"), categoryId));
+        Long count = entityManager.createQuery(countQuery).getSingleResult();
+
         return new PageImpl<>(content, pageable, count);
     }
 }
